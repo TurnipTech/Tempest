7601315fffd72d5bbe1839a67ac2ed13836443e9
Harry Acourt <harryacourt@gmail.com>
2025-06-13 11:21:34 +0100

    Add ktlint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,32 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+tasks.register("ktlintCheckAll") {
+    group = "verification"
+    description = "Run ktlint check on all modules"
+    dependsOn(subprojects.map { "${it.path}:ktlintCheck" })
+}
+
+tasks.register("ktlintFormatAll") {
+    group = "formatting"
+    description = "Run ktlint format on all modules"
+    dependsOn(subprojects.map { "${it.path}:ktlintFormat" })
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("1.0.1")
+        android.set(true)
+        ignoreFailures.set(false)
+
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+    }
 }
