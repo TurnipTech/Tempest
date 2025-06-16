9708ac05d4b4abf7d142f3dace2686438d595ebb
Harry Acourt <harryacourt@gmail.com>
2025-06-16 16:47:25 +0100

    Fix search bar padding

--- features/search_location/src/main/java/com/harry/location/ui/components/LocationSearchBar.kt
+++ features/search_location/src/main/java/com/harry/location/ui/components/LocationSearchBar.kt
@@ -1,9 +1,12 @@
 package com.harry.location.ui.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
@@ -15,6 +18,7 @@ import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -34,6 +38,10 @@ fun LocationSearchBarComponent(
     modifier: Modifier = Modifier,
     content: @Composable ColumnScope.() -> Unit,
 ) {
+    val animatedPadding by animateDpAsState(
+        targetValue = if (expanded) 0.dp else 16.dp,
+        label = "searchBarPadding"
+    )
     SearchBar(
         inputField = {
             SearchBarDefaults.InputField(
@@ -78,7 +86,9 @@ fun LocationSearchBarComponent(
         },
         expanded = expanded,
         onExpandedChange = onExpandedChange,
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = animatedPadding),
         windowInsets = SearchBarDefaults.windowInsets,
         colors = SearchBarDefaults.colors(),
         content = content,
