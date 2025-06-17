d47ed0ba22d43d081ed44302fd2ff6d7d3fa267c
Harry Acourt <harryacourt@gmail.com>
2025-06-17 14:03:12 +0100

    Remove old theme from app module

--- core/design_system/src/main/java/com/harry/design/Theme.kt
+++ core/design_system/src/main/java/com/harry/design/Theme.kt
@@ -27,7 +27,6 @@ private val LightColorScheme =
 @Composable
 fun TempestTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
